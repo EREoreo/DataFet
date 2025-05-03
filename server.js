102c989ea5a65bf2271bfa7c78ab4774f3130d3b
EREoreo <error323269@gmail.com>
2025-05-04 01:16:17 +0500

    массовый расчет

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 app.use(express.json());
 
 const __dirname = path.resolve();
